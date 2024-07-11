a53129039d1047907a9a5f5459666dadb59413b0
jhonUTEA <032296c@utea.edu.pe>
2024-07-10 20:51:58 -0500

    iniciando github

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
-                            <!-- hola -->
+                            <!-- solucion de inicializacion -->
                         </div>
                     @endif
 
