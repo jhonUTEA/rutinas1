f0fd661c5f1b0223a1a66e4b1241d370eb1df6e0
jhonUTEA <032296c@utea.edu.pe>
2024-07-26 23:40:44 -0500

    habilitando Agregra Rutina

--- resources/views/rutinas/crear.blade.php
+++ resources/views/rutinas/crear.blade.php
@@ -1,53 +1,48 @@
 @extends('layouts.panel')
 @section('content')
-
-
-<div class="row mt-5">
+  <div class="row mt-5">
     <div class="col-xl-12 mb-5 mb-xl-0">
       <div class="card shadow">
         <div class="card-header border-0">
           <div class="row align-items-center">
             <div class="col">
-              <h3 class="mb-0">Nueva Tarea</h3>
+              <h3 class="mb-0">Nueva Rutina</h3>
             </div>
-            <div class="col text-right ">
-             <a href="{{ url('/rutinas')}}" class="btn btn-sm btn-success" >
-                <i class="ti ti-arrow-left"></i>
-                Salir</a>
+            <div class="col text-right">
+              <a href="{{ url('/rutinas')}}" class="btn btn-sm btn-success">
+                <i class="fas fa-chevron-left"></i>
+                Regresar</a>
             </div>
           </div>
         </div>
-    </div>
-</div>
-</div>
-
-<div class="row mt-3">
-<div class="col-xl-12 mb-5 mb-xl-0">
-    <div class="card shadow-lg">
+        
         <div class="card-body">
-            <form role="form" action="{{ url('/rutinas')}}" method="POST">
+
+            @if ($errors->any())
+                @foreach($errors->all() as $error)
+                    <div class="alert alert-danger" role="alert">
+                        <i class="fas fa-exclamation-triangle"></i>
+                        <strong>Por favor!</strong> {{ $error}}
+                    </div>
+                @endforeach
+
+            @endif
+
+
+            <form action="{{ url('/rutinas')}}" method="POST">
                 @csrf
-                
-                <div class="form-group col-md-6">
-                    
+                <div class="form-group">
+                    <label for="Descripcion_rutina">Nombre de la Rutina</label>
                     <input type="text" name="Descripcion_rutina" class="form-control" value="{{old('Descripcion_rutina')}}"  required>
-                    <label for="Descripcion_rutina">Describe la rutina</label>
                 </div>
 
-                <div class="form-group col-md-2">
-                    <input type="date" placeholder="Vence_rutina" name="Vence_rutina" class="form-control" value="{{old('Vence_rutina')}}"  required>
-                     
+                <div class="form-group">
+                    <label for="Vence_rutina">Vencimiento de Rutina</label>
+                    <input type="date" name="Vence_rutina" class="form-control" value="{{old('Vence_rutina')}}">
                 </div>
-                <div class="form-group col-md-4">
-                    <label for="Vence_rutina">Vencimiento de la rutina</label>
-                </div>
-                
-            
-
-                <button type="submit" class="btn btn-sm btn-primary">Crear Rutina</button>
+                <button type="submit" class="btn btn-sm btn-primary">Guardar Rutina</button>
             </form>
         </div>
       </div>
-    </div>
-    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/*Se definen rutas en una aplicación Laravel.
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes();/* Esta línea configura las rutas de autenticación proporcionadas por Laravel,
+                  que incluyen registro, login, y recuperación de contraseñas*/
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
